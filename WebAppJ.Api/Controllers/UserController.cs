e1cea2a63753db6d3d6c56ee2e25daca967a3f5a
VovaDegtyarev <vladimir1860921@gmail.com>
2022-06-23 12:25:58 +0300

    Добавьте файлы проекта.

---
+++ WebAppJ.Api/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppJ.BL.Interfaces;
+
+namespace WebAppJ.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService userService;
+        private readonly ILogger logger;
+
+        public UserController(IUserService userService, ILogger<UserController> logger)
+        {
+            this.logger = logger;
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult GetUser()
+        {
+            var user = userService.GetUser();
+            logger.LogInformation("Method GetUser called");
+
+            return Ok(user);
+        }
+
+    }
+}
